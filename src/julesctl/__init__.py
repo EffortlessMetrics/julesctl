c8e8951cb809583c00dc026c5e023dbd7afa0b95
Steven Zimmerman, CPA <15812269+EffortlessSteven@users.noreply.github.com>
2026-09-18 23:25:45 -0400

    feat: expose stable Python client

--- src/julesctl/__init__.py
+++ src/julesctl/__init__.py
@@ -1,7 +1,8 @@
 """julesctl public package."""
 
+from .client import JulesClient
 from .controller import JulesController
 from .domain.models import DispatchSpec
 
-__all__ = ["DispatchSpec", "JulesController"]
+__all__ = ["DispatchSpec", "JulesClient", "JulesController"]
 __version__ = "0.1.0a1"
